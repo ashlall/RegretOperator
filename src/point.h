a6a810143af677e64f92bc6c16c79a205dc19faa
rinkerm <31450488+rinkerm@users.noreply.github.com>
2018-06-12 15:18:53 -0400

    Add files via upload

--- src/point.h
+++ src/point.h
@@ -1,5 +1,25 @@
-#ifndef POINT_H
-#define POINT_H
+//==========================================================================================
+//Copyright 2018 ©, 2018 Minh Do, Hiep phan, Quang, Nguyen, Matthew Rinker, Ashwin Lall 
+//
+//This file is a part of the RegretOperator project.
+//
+//The RegretOperator project is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    The RegretOperator project is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//along with this program. If not, see <http://www.gnu.org/licenses/>.
+//==========================================================================================
+
+
+#ifndef REGRETOPERATOR_SRC_POINT_H_
+#define REGRETOPERATOR_SRC_POINT_H_
 
 struct point
 {
