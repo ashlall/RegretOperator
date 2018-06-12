0298915526cefe92b1378e1d328378e2bb6994c7
rinkerm <31450488+rinkerm@users.noreply.github.com>
2018-06-12 15:38:04 -0400

    Add files via upload

--- src/point.cpp
+++ src/point.cpp
@@ -1,15 +1,40 @@
-#include "point.h"
+//==========================================================================================
+//Copyright 2018 ©, 2018 Minh Do, Hiep phan, Quang, Nguyen, Matthew Rinker, Ashwin Lall 
+//
+//This file is a part of the RegretOperator project.
+//
+//The RegretOperator project is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    The RegretOperator project is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//along with this program. If not, see <http://www.gnu.org/licenses/>.
+//==========================================================================================
 
-#ifndef POINTCPP
-#define POINTCPP
+
+#include "point.h"
 
 #include <iostream>
 #include <stdio.h>
 
 using namespace std;
 
+
 double dot(struct point p, double* w)
 {
+// This struct stores a point and its component in a d-dimensional space
+// Example:
+// 		struct point p;
+
+// Returns the dot product of vector p and w.
+// p is the data point 
+// w is the tuple representing a utilify function
 	double product = 0.0;
 	int i;
 
@@ -19,9 +44,11 @@ double dot(struct point p, double* w)
 	return product;
 }
 
-// finds the point in p farthest in the direction v
+
 struct point maxPoint(int N, struct point *p, double *v)
 {
+// Finds farthest in direction v. 
+// This method is intended to find the boundary points
 	int i, maxIndex = 0;
 	double max = 0.0;
 	
@@ -36,7 +63,7 @@ struct point maxPoint(int N, struct point *p, double *v)
 }
 		
 
-
+// Prints the components of a point
 double print(struct point p)
 {
 	int i;
@@ -44,13 +71,22 @@ double print(struct point p)
 		printf("%lf ", p.a[i]);
 	printf("\n");
 	
-	//Actually, what the heck should this return? Currently, I'm just placing an arbitrary value here, so note that THIS IS BROKEN
 	return 0;
 	
 }
 
+
 int pointcmp(const void *a, const void *b) 
 {
+// Compares two points lexicographically.
+// Return values:
+//		-1: a < b
+// 		 1: a > b
+// 		 0: a = b
+// Example:
+// 		p1(1,2,3,4,6) and p2(1,2,3,5,5)
+// 		=> p1 < p2 because p1[i] = p2[i] for 1 <= i <= 3
+// 		               and p1[4] < p2[4]
 	int i;
 
 	for(i = 0; i < ((struct point*)a)->d; ++i)
@@ -63,22 +99,10 @@ int pointcmp(const void *a, const void *b)
 }
 
 
-
-/*
-//This is commented out, because I implement a different sorting algorithm in newAlgorithmTest.cpp
-
-void sort(struct point* p, int n)
-{
-	qsort(p, n, sizeof(struct point), pointcmp);
-}*/
-
-
-
-// checks if p1 dominates p2
-// assumes p1.d == p2.d 
-// points dominate themselves
 int dominates(struct point p1, struct point p2)
 {
+// Checks if p1 dominates p2
+// p1 dominates p2 when p1[i] >= p2[i] for all 1 <= i <= d
 	int i;
 
 	for(i = 0; i < p1.d; ++i)
@@ -88,10 +112,11 @@ int dominates(struct point p1, struct point p2)
 	return 1;
 }
 
-// checks if two points are equal
+
 int equals(struct point p1, struct point p2)
 {
+// Checks if two points are equal.
+// In other words, if pointcamp returns 0.
 	return pointcmp(&p1, &p2) == 0;
 }
 
-#endif 
